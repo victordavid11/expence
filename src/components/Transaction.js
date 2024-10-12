d3b9435fde4a33b376b60566e3a0bdf4e3ef44cd
victordavid11 <okaravictor90@gmail.com>
2024-10-12 01:51:06 +0100

    build

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,16 +4,8 @@ import { GlobalContext } from '../context/GlobalState';
 //Money formatter function
 function moneyFormatter(num) {
   let p = num.toFixed(2).split('.');
-  return (
-    '$ ' +
-    p[0]
-      .split('')
-      .reverse()
-      .reduce(function (acc, num, i, orig) {
-        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
-      }, '') +
-    '.' +
-    p[1]
+  return ('$ ' +p[0].split('').reverse().reduce(function (acc, num, i, orig) 
+      { return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;}, '') + '.' + p[1]
   );
 }
 
@@ -24,7 +16,8 @@ export const Transaction = ({ transaction }) => {
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text} <span>{sign}{moneyFormatter(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+      {transaction.text} <span>{sign}{moneyFormatter(transaction.amount)}</span>
+      <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>
   )
 }
